6122fa783ed1bd275eed0ddbd919865b05e5446c
JP White <jpwhite3@gmail.com>
2023-01-30 13:23:54 -0500

    Refactoring

--- src/build.py
+++ src/build.py
@@ -1,20 +1,13 @@
-import argparse, os, shutil, logging
+import argparse, os
 from pathlib import Path
 from typing import List
 from collections import OrderedDict
 from PIL import Image
 from pillow_heif import register_heif_opener
 from jinja2 import Environment, FileSystemLoader
-from aws import s3_key_exists, upload_to_s3
 
 register_heif_opener()
 
-logging.basicConfig(
-    level=os.environ.get("LOGLEVEL", "INFO").upper(),
-    format="[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s",
-)
-
-LOG = logging.getLogger(__name__)
 OUTPUT_DIR = Path("./dist")
 OUTPUT_GALLERY_DIR = Path("./dist/images")
 SEARCH_GLOB_PATTERNS = [
@@ -28,7 +21,7 @@ template = environment.get_template("index.html")
 
 
 def make_thumbnail(image_path, thmb_path):
-    LOG.debug(f"Creating thumbnail for {image_path}")
+    print(f"Creating thumbnail for {image_path}")
     with Image.open(image_path) as image:
         image.thumbnail((350, 350))
         image.save(thmb_path, format="webp")
@@ -57,11 +50,14 @@ def main():
     input_path = Path(args.inputdir)
     gallery_data = OrderedDict()
     for source_file_path in get_file_list(input_path):
+        print(f"Processing {source_file_path}")
         target_thmb_path = source_file_path.parent.joinpath(
             ".thmb", source_file_path.name
         ).with_suffix(".webp")
 
-        if not target_thmb_path.exists():
+        if target_thmb_path.exists():
+            print(f"Found existing thumbnail: {target_thmb_path}")
+        else:
             target_thmb_path.parent.mkdir(parents=True, exist_ok=True)
             make_thumbnail(source_file_path, target_thmb_path)
 
